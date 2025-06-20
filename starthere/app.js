ab6fe169af9738789189133f56255eec889c838d
Yaashvin Prem-Kumar <yaashvinpremkumar@gmail.com>
2025-06-20 09:32:36 +0000

    [automatic commit] 2025-06-20 09:32:36 UTC 202.7.247.40

--- starthere/app.js
+++ starthere/app.js
@@ -86,13 +86,16 @@ app.post('/login', async (req, res) => {
       [username, password]
     );
 
-    if (rows.length > 0) {
-      req.session.user = rows[0];
-      const redirectUrl = rows[0].role === 'owner' ? '/owner-dashboard.html' : '/walker-dashboard.html';
-      return res.redirect(redirectUrl);
-    } else {
-    res.status(401).send('Invalid credentials');
-    }
+if (rows.length > 0) {
+    req.session.user = rows[0];
+    const redirectUrl = rows[0].role === 'owner'
+      ? '/owner-dashboard.html'
+      : '/walker-dashboard.html';
+    return res.redirect(redirectUrl);
+  } else {
+    return res.status(401).send('Invalid credentials');
+  }
+
   } catch (err) {
     res.status(500).send('Login error');
   }

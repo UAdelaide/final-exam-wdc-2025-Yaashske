8734ea43b3ce0caaa05ebeb8d283b790fb5f9dc8
Yaashvin Prem-Kumar <yaashvinpremkumar@gmail.com>
2025-06-20 09:27:28 +0000

    [automatic commit] 2025-06-20 09:27:28 UTC 202.7.247.40

--- starthere/app.js
+++ starthere/app.js
@@ -81,22 +81,24 @@ app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const [rows] = await db.execute(
-      'SELECT * FROM Users WHERE username = ? AND password_hash = ?',
-      [username, password]
-    );
+  const [rows] = await db.execute(
+    'SELECT * FROM Users WHERE username = ? AND password_hash = ?',
+    [username, password]
+  );
 
-    if (rows.length > 0) {
-      req.session.user = rows[0];
-      const redirectUrl = rows[0].role === 'owner' ? '/owner-dashboard.html' : '/walker-dashboard.html';
-      return res.redirect(redirectUrl);
-    } else {
-      res.status(401).send('Invalid credentials');
-    }
-  } catch (err) {
-    res.status(500).send('Login error');
+  if (rows.length > 0) {
+    req.session.user = rows[0];
+    const redirectUrl = rows[0].role === 'owner'
+      ? '/owner-dashboard.html'
+      : '/walker-dashboard.html';
+    return res.redirect(redirectUrl);
+  } else {
+    return res.status(401).send('Invalid credentials');
   }
-});
+
+} catch (err) {
+  res.status(500).send('Login error');
+}
 
 // logout route
 app.get('/logout', (req, res) => {
